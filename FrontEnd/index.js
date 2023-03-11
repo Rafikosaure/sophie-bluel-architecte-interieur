4393408c9adabec6a4d49f8959a33c8b4027691e
Rafik Ben Sadi <rafikbensadi@live.fr>
2023-03-11 15:01:36 +0100

    Modale : première implémentation des fonctions d'ouverture et de fermeture de la modale.

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -146,3 +146,32 @@ headerLogoutButton.addEventListener("click", function () {
     localStorage.removeItem("token");
     window.location.replace("index.html");
 });
+
+
+// Gestion de la boîte modale
+let modal = null;
+// Fonction d'ouverture de la modale
+const openModal = function(e) {
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute("href"));
+    target.style.display = null;
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", true);
+    modal = target;
+    modal.addEventListener("click", closeModal);
+};
+// Fonction de fermeture de la modale
+const closeModal = function(e) {
+    if (modal === null) {
+        return
+    };
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    modal.removeAttribute("aria-modal");
+    modal.removeEventListener("click", closeModal);
+    modal = null;
+}
+document.querySelectorAll(".js-modal").forEach(a => {
+    a.addEventListener("click", openModal);
+});
